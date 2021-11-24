2e861c98b0155ceb64ada3dd2681d8bbe00d9285
Warspiteful <jalmendral24@gmail.com>
2021-11-24 18:37:27 -0500

    Dragable Object Proof of Concept

--- New folder/Scenes/MicroGames/Batch1/mgCleanRoom.gd
+++ New folder/Scenes/MicroGames/Batch1/mgCleanRoom.gd
@@ -6,6 +6,8 @@ extends Micro_Game
 # var b = "text"
 
 
+var selected = false;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,3 +16,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _physics_process(delta):
+	
+	if selected:
+		global_position = lerp(global_position,get_global_mouse_position(), 25 * delta)
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	print("CHECK")
+	if Input.is_action_just_pressed("click"):
+		print("HELLO")
+		selected = true
+		
