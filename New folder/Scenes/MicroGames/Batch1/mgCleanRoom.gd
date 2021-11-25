08a96ddf44edc32a3c2a94b99efb4326fdeca0c1
Warspiteful <jalmendral24@gmail.com>
2021-11-24 20:22:09 -0500

    Drag and Drop Finished

--- New folder/Scenes/MicroGames/Batch1/mgCleanRoom.gd
+++ New folder/Scenes/MicroGames/Batch1/mgCleanRoom.gd
@@ -1,36 +1,10 @@
 extends Micro_Game
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-var selected = false;
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	AudioManager.play_music("mario-tenis-exhibition-court.ogg")
 
+func _process(delta):
+	if(get_tree().get_nodes_in_group("trash") == []):
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		game_cleared()
 
-
-func _physics_process(delta):
-	
-	if selected:
-		global_position = lerp(global_position,get_global_mouse_position(), 25 * delta)
-
-
-func _on_Area2D_input_event(viewport, event, shape_idx):
-
-	if Input.is_action_just_pressed("click"):
-		selected = true
-		
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT:
-			selected = false
-		
---
+++ New folder/Scenes/MicroGames/Batch1/mgCleanRoomDrag.gd
@@ -0,0 +1,44 @@
+extends Micro_Game
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+var selected = false;
+
+var rest_nodes = []
+var rest_point
+func _ready():
+	rest_nodes = get_tree().get_nodes_in_group("zone")
+	rest_point = null
+
+	
+func _physics_process(delta):
+	
+	if selected:
+		global_position = lerp(global_position,get_global_mouse_position(), 25 * delta)
+
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+
+	if Input.is_action_just_pressed("click"):
+		selected = true
+		
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			selected = false
+			var shortest_dist = 20;
+			for child in rest_nodes:
+
+				var distance = global_position.distance_to(child.global_position)
+				if distance < shortest_dist:
+					child.select()
+					rest_point = child.global_position
+					shortest_dist = distance
+					AudioManager.play_sfx_name("correct.wav")
+					queue_free()
+		
