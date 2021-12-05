59116ee111da7e2e633c1f21d1c72b1d6efd7c68
Warspiteful <jalmendral24@gmail.com>
2021-12-05 03:43:56 -0500

    Phone Alarm Art Implemented

--- New folder/Scenes/MicroGames/Batch2/mgPhoneAlarm.gd
+++ New folder/Scenes/MicroGames/Batch2/mgPhoneAlarm.gd
@@ -2,6 +2,10 @@ extends Micro_Game
 
 onready var lock = $lock
 
+func _init():
+	time = 5
+	hint_string = "Unlock your Phone!"
+	
 func _ready():
 	AudioManager.play_music("mario-tenis-exhibition-court.ogg")
 
